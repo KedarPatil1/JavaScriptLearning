e0bf6fdf5c656f7c5769f51fa5044962273b9f04
kedar <patilks029@gmail.com>
2022-10-05 17:37:35 +0530

    Demo JavaScript Files
    Demo JavaScript Files

---
+++ Demo.js
@@ -0,0 +1,5 @@
+console.log("Hello");
+console.log("Mine name is Kedar Patil");
+console.info("This gives information");
+console.warn("This gives Warning");
+console.error("This gives Error") //all these are JavaScript Methods
