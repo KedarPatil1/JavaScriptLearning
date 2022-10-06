8407590ced154ab69eb3036bf139af0a45c83027
kedar <patilks029@gmail.com>
2022-10-06 22:37:42 +0530

    Assignment_02

---
+++ Assignment_02.js
@@ -0,0 +1,23 @@
+var firstName="Kedar";
+var lastName="Patil";
+var city="Jalna";
+var state="Maharashtra";
+console.log(firstName,lastName,city,state);
+
+var x="Kedar";
+var y="Patil"
+console.log("Start Name", x , y);
+var temp=x;
+x=y;
+y=temp;
+console.log("End Name", x, y);
+
+var a=100;
+var b=200;
+var c=300;
+console.log("Before swapping", a , b,c);
+temp1=a; //temp1=100
+a=c; //a=200
+c=b; //b=100
+c=temp1; //c=200
+console.log("After swapping", a,b,c);
