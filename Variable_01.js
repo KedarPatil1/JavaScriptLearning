d81812e8f793aadd6c3954a6fb4843ed0e153111
kedar <patilks029@gmail.com>
2022-10-06 21:57:50 +0530

    Assignment on JavaScript

---
+++ Variable_01.js
@@ -0,0 +1,23 @@
+var container; //variable declaration
+container = "Pen" //variable initialization
+console.log(container)
+var x = "Value" //variable declaration and initialization
+const a=5;
+const b=8;
+let c= a*b;
+console.log(c)
+
+container="pensile"
+console.log(container)
+
+var myNumber;
+myNumber=200;
+console.log(myNumber)
+
+var myLove="I like JavaScript"
+console.log(myLove)
+
+var temp=200;
+console.log(temp);
+temp="JavaScript"
+console.log(temp)
