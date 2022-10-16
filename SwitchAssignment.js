501ae10877c9c37040fe528df3589fb94d6d20bf
kedar <patilks029@gmail.com>
2022-10-16 21:45:09 +0530

    Switch assignment on month 14/10/2022

---
+++ SwitchAssignment.js
@@ -0,0 +1,43 @@
+var monthInYear = 12;
+switch (monthInYear) {
+    case 1:
+        console.log("Jan")
+        break;
+    case 2:
+        console.log("Feb")
+        break;
+    case 3:
+        console.log("Mar")
+        break;
+    case 4:
+        console.log("April")
+        break;
+    case 5:
+        console.log("May")
+        break;
+    case 6:
+        console.log("June")
+        break;
+    case 7:
+        console.log("Jully")
+        break;
+    case 8:
+        console.log("August")
+        break;
+    case 9:
+        console.log("Sep")
+        break;
+    case 10:
+        console.log("Oct")
+        break;
+    case 11:
+        console.log("Nov")
+        break;
+    case 12:
+        console.log("Dec")
+        break;
+
+    default:
+        console.log("Please enter valid month")
+        break;
+}
