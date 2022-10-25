3560970f7d768df0edb1c8ed13f6f047a720ab0d
kedar <patilks029@gmail.com>
2022-10-25 22:37:03 +0530

    Code and Assignment on while and do while loop held on 18/10/2022
    Assignment is in the WhileLoopAssignment.js file

---
+++ WhileLoop.js
@@ -0,0 +1,19 @@
+// var x = 1;
+// while (x <= 10) {
+
+//     var y = x * 5
+//     x++
+//     console.log(y)
+// }
+var count=5;
+while(count<=50){
+       console.log(count)
+     count=count+5;
+ 
+}
+
+// var z=10;
+// while (z>=0) {
+//     z--
+//     console.log(z)
+// }
---
+++ WhileLoopAssignment.js
@@ -0,0 +1,49 @@
+// console.log(`Numbers from 5 to 15 using while loop`)
+// var x=5;
+// while (x<=15) {
+//     console.log(x);
+//     x++;
+// }
+// console.log(`Numbers from 50 to 40 using while loop`)
+// var y=50;
+// while (y>=40) {
+//     console.log(y);
+//     y--;
+// }
+// console.log(`First 15 odd numbers using while loop`)
+// var x=1;
+// while (x<30) {
+//     console.log(x);
+//     x=x+2
+
+// }
+// console.log(`First 10 Even numbers using while loop`)
+// var x=0;
+// while (x<=20) {
+//     console.log(x);
+//     x=x+2;
+    
+// }
+// console.log(`Table of 5 using while loop`)
+
+// var x=5;
+// while (x<=50) {
+//     console.log(x);
+//     x=x+5
+// }
+
+// console.log(`Table of 5 using while loop`)
+// console.log(`Table of 5 using while loop`)
+
+// var x=10;
+// while (x<=100) {
+//     console.log(x);
+//     x=x+10
+// }
+console.log(`Reversing the string from 100 to 0`)
+
+var x=100;
+while (x>=0) {
+    console.log(x);
+    x--
+}
